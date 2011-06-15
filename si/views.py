dc0a5c3ea9dd96a0123b3b0e93a9e5b3c8676dac
Yasuaki Narita <yasuaki_n@mti.biglobe.ne.jp>
2011-06-16 06:43:48 +0900

    list index out of range...

--- si/views.py
+++ si/views.py
@@ -16,5 +16,34 @@ def detail(request, cpu_name, ver):
 	si = Si.objects.filter(cpu=cpu.id, ver=ver)[0]
 	cpu_list = Cpu.objects.order_by('name')
 	si_list = Si.objects.filter(cpu=cpu.id).order_by('start_date')
+	atendfbu_list = SiAttendFbu.objects.filter(si=si.id,valid=True)
 	fburel_hist = get_git_hist(gitdir,si.git_branch,si.git_branch)
-	return render_to_response('si/detail.html', {'cpu':cpu, 'si':si, 'cpu_list':cpu_list, 'si_list':si_list, 'fburel_hist':fburel_hist}, context_instance=RequestContext(request))
+	
+	# generate phasestatus_list{atendfbu.id}{phase} = {SIPhaseStatus's row}
+	phasestatus_list = {}
+	for afbu in atendfbu_list:
+		p = {}
+		p['FR'] = SiPhaseStatus.objects.filter(si=si.id,fbu=afbu.fbu.id,phase=u'FR')[0]
+		p['FC'] = SiPhaseStatus.objects.filter(si=si.id,fbu=afbu.fbu.id,phase=u'FC')[0]
+		p['SC'] = SiPhaseStatus.objects.filter(si=si.id,fbu=afbu.fbu.id,phase=u'SC')[0]
+		phasestatus_list[afbu.fbu.name] = p
+	p = {}
+	p['DL'] = SiPhaseStatus.objects.filter(si=si.id,phase=u'DL')[0]
+	p['CO'] = SiPhaseStatus.objects.filter(si=si.id,phase=u'CO')[0]
+	p['BC'] = SiPhaseStatus.objects.filter(si=si.id,phase=u'BC')[0]
+	p['SR'] = SiPhaseStatus.objects.filter(si=si.id,phase=u'SR')[0]
+	phasestatus_list[0] = p
+
+	return render_to_response(
+		'si/detail.html', {
+			'cpu':cpu,
+			'si':si,
+			'cpu_list':cpu_list,
+			'si_list':si_list,
+			'atendfbu_list':atendfbu_list,
+			'phasestatus_list':phasestatus_list,
+			'fburel_hist':fburel_hist,
+			'si_phase':SI_PHASES,
+		},
+		context_instance=RequestContext(request)
+	)
